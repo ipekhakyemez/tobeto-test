065751adc03e28c8d8f3f5824609814a968a4437
ipekhakyemez <88480940+ipekhakyemez@users.noreply.github.com>
2023-10-25 21:32:05 +0300

    Update maas.py

--- odevler/odev1/maas.py
+++ odevler/odev1/maas.py
@@ -1,1 +1,7 @@
+#Kullanicidan alinan maas ve zam orani ile yeni maas hesaplanmasi
 
+maas = float(input("Maaşınızı giriniz :"))
+zam_orani = float(input("Zam oranını giriniz :"))
+
+yeni_maas = ( maas * zam_orani/100 ) + maas
+print(yeni_maas)
