15c533075176520acac873f43066ea091f5448a4
ipekhakyemez <88480940+ipekhakyemez@users.noreply.github.com>
2023-10-28 21:35:33 +0300

    Update fibonacci.py

--- odevler/odev2/fibonacci.py
+++ odevler/odev2/fibonacci.py
@@ -1,1 +1,12 @@
+sayi = int(input("sayı adedini giriniz: "))
+fibonacci = [1,1]
 
+if sayi > 19:
+    for i in range (2,sayi):
+   
+        sonEleman = fibonacci [i-1]  + fibonacci [i-2]
+        fibonacci.append(sonEleman)
+    print(fibonacci)
+
+else: 
+    print("20 elemandan az girilemez.")
