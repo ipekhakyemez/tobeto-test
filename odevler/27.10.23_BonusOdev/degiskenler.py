826f8ff86b1ee73bacee4f0f2b704c02e305b25a
ipekhakyemez <88480940+ipekhakyemez@users.noreply.github.com>
2023-10-30 22:09:19 +0300

    Update degiskenler.py

--- odevler/27.10.23_BonusOdev/degiskenler.py
+++ odevler/27.10.23_BonusOdev/degiskenler.py
@@ -1,1 +1,23 @@
+baslik = "HABERİNİZ OLSUN"   
+vade = 12                    
+faizOrani1 = 1.47            
+faizOrani2 = 1.44
 
+print(baslik)
+print(type(baslik))
+print(type(vade))
+print(type(faizOrani1))
+
+
+mesaj = "Hoşgeldin"
+musteriAdi = "İpek"
+sonucMesaj = mesaj + " "+ musteriAdi + "!"
+
+print(sonucMesaj)
+
+sayi1 = 20
+sayi2 = 40
+print(sayi1+sayi2)
+
+
+print(sonucMesaj)
